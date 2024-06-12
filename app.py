463cb96fb9dbf832287c0380c273d86a7a5bae44
Nisflei Galoni <88749138+Nisflei@users.noreply.github.com>
2024-06-12 14:42:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS..</font></h1></center> '
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
